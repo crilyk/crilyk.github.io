36b44565bfa540317a0e7ee831348d36639bdb1e
crilyk <3145077531@qq.com>
2024-07-18 14:26:27 +0800

    Site updated: 2024-07-18 14:26:27

--- js/backgroundize.js
+++ js/backgroundize.js
@@ -1,12 +1,10 @@
 
-document
-  .querySelector('#web_bg')
-  .setAttribute('style', `background-image: ${document.querySelector('.banner').style.background.split(' ')[0]};position: fixed;width: 100%;height: 100%;z-index: -1;background-size: cover;`);
-
-document
-  .querySelector("#banner")
-  .setAttribute('style', 'background-image: url()')
-
-document
-  .querySelector("#banner .mask")
-  .setAttribute('style', 'background-color:rgba(0,0,0,0)')
+const bannerContainer = $("#banner");
+const viewBg = $("#web_bg");
+const bannerMask = $("#banner .mask");
+const bg = $(bannerContainer).css("background-image");
+$(viewBg).css("background-image", bg);
+$(bannerContainer).css("background-image", "url()");
+const color = $(bannerMask).css("background-color");
+$(bannerMask).css("background-color", `rgba(0,0,0,0)`);
+$(viewBg).css("background-color", color);
