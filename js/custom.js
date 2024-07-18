db9ec11f4c3b04be8420295b62d36bbb4ddfe29c
crilyk <3145077531@qq.com>
2024-07-18 15:01:58 +0800

    Site updated: 2024-07-18 15:01:58

--- js/custom.js
+++ js/custom.js
@@ -90,7 +90,7 @@ function(){
 
 
 //鼠标点击特效
-var a_idx=0;function delay(){$(".buryit").removeAttr("onclick")}jQuery(document).ready((function(a){a("body").click((function(t){var e=new Array("富强","民主","文明","和谐","自由","平等","公正","法治","爱国","敬业","诚信","友善"),o=a("<span/>").text(e[a_idx]);a_idx=(a_idx+1)%e.length;var n=t.pageX,i=t.pageY;o.css({"z-index":5,top:i-20,left:n,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}),a("body").append(o),o.animate({top:i-180,opacity:0},3e3,(function(){o.remove()}))})),setTimeout("delay()",2e3)}));
+var a_idx=0;function delay(){$(".buryit").removeAttr("onclick")}jQuery(document).ready((function(a){a("body").click((function(t){var e=new Array("走", "和我", "去看", "星星", "呀"),o=a("<span/>").text(e[a_idx]);a_idx=(a_idx+1)%e.length;var n=t.pageX,i=t.pageY;o.css({"z-index":5,top:i-20,left:n,position:"absolute","font-weight":"bold",color:"rgb("+~~(255*Math.random())+","+~~(255*Math.random())+","+~~(255*Math.random())+")"}),a("body").append(o),o.animate({top:i-180,opacity:0},3e3,(function(){o.remove()}))})),setTimeout("delay()",2e3)}));
 //# sourceMappingURL=/sm/29e1be1b129d571fa38b27b554a2941e7c80143af182baf2e157a070272cb3c1.map
 		// 	var a_idx=0;
 		// 	jQuery(document).ready(function($){
