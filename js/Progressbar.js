32f7c27356bc30a020ecb5e5b6b4d9f246ddd7ed
crilyk <3145077531@qq.com>
2025-03-23 18:34:49 +0800

    Site updated: 2025-03-23 18:34:48

--- js/Progressbar.js
+++ js/Progressbar.js
@@ -1,6 +1,31 @@
+var lList = document.querySelectorAll('.gutter pre'); // 获取所有的 .gutter pre
+var rList = document.querySelectorAll('.code pre');   // 获取所有的 .code pre
 
-var l=document.querySelector('.gutter pre')
-var r=document.querySelector('.code pre')
-r.addEventListener('scroll',function(){
-  l.scrollTop = r.scrollTop
-})
+if (lList.length !== rList.length) {
+  console.error('Error: The number of .gutter pre and .code pre elements does not match.');
+}
+
+lList.forEach((l, index) => {
+  let r = rList[index]; // 获取对应的右侧 pre 元素
+
+  if (!r) return; // 防止 r 元素不存在
+
+  let isSyncingLeftScroll = false;
+  let isSyncingRightScroll = false;
+
+  // 监听左侧代码块的滚动
+  l.addEventListener('scroll', () => {
+    if (isSyncingLeftScroll) return;
+    isSyncingRightScroll = true;
+    r.scrollTop = l.scrollTop;  // 右侧同步滚动
+    isSyncingRightScroll = false;
+  });
+
+  // 监听右侧代码块的滚动
+  r.addEventListener('scroll', () => {
+    if (isSyncingRightScroll) return;
+    isSyncingLeftScroll = true;
+    l.scrollTop = r.scrollTop;  // 左侧同步滚动
+    isSyncingLeftScroll = false;
+  });
+});
